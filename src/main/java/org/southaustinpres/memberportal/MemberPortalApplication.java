4525324f9b13d8f76982779923d9dba04428b8be
Jacob R. Rothenbuhler <jake@encodify.net>
2020-03-25 09:51:30 -0500

    Base application from Spring Initializr

---
+++ src/main/java/org/southaustinpres/memberportal/MemberPortalApplication.java
@@ -0,0 +1,13 @@
+package org.southaustinpres.memberportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/southaustinpres/memberportal/MemberPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.southaustinpres.memberportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
